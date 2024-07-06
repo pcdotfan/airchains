2528c2c661df39ae3161112c18be449355b4bb89
Weng Weiyu <weiyu.weng@u.nus.edu>
2024-07-06 14:20:31 +0800

    Update airchain.sh

--- airchain.sh
+++ airchain.sh
@@ -80,15 +80,6 @@ else
     sudo apt install -y wget
 fi
 
-# 检查是否已安装 go
-if command -v go >/dev/null 2>&1; then
-    echo "go 已安装，跳过安装步骤。"
-else
-    echo "下载并安装 Go..."
-    wget -c https://golang.org/dl/go1.22.4.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
-    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-    source ~/.bashrc
-fi
 
 # 验证安装后的 Go 版本
 echo "当前 Go 版本："
@@ -124,9 +115,7 @@ EOF
     sudo systemctl start wasmstationd
     
     cd
-wget https://github.com/airchains-network/tracks/releases/download/v0.0.2/eigenlayer
-sudo chmod +x eigenlayer
-sudo mv eigenlayer /usr/local/bin/eigenlayer
+# install eigenlayer-cli manually https://github.com/Layr-Labs/eigenlayer-cli
 # 定义文件路径
 KEY_FILE="$HOME/.eigenlayer/operator_keys/wallet.ecdsa.key.json"
 # 检查文件是否存在
